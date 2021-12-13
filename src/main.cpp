e72195a720901572cf4188506cac7ef6f177e933
onart <onart97@naver.com>
2021-12-13 16:59:48 +0900

    리소스 포함용 xxd-2 (클릭만으로 가능)

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,17 @@
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <filesystem>
+
+char buf[256];
 
 int main(int argc, char* argv[]) {
-	if (argc < 2) return 0;
-	FILE* fp;
-	fopen_s(&fp, argv[1], "rb");
-	if (!fp)return 0;
-	printf("unsigned char ");
-	int i = 0, j;
-	for (; argv[1][i] != '.'; i++) {
-		printf("%c", argv[1][i]);
+	for (auto& p : std::filesystem::recursive_directory_iterator(".")) {
+		auto fn = p.path().filename();
+		if (fn.extension() == ".txt" || fn == "myxxd.exe" || fn == std::filesystem::path(argv[0]).filename()) { continue; }
+		std::string fName = fn.string();
+		fn.replace_extension("txt");
+		sprintf_s(buf, 256, "myxxd.exe %s > %s", fName.c_str(), fn.string().c_str());
+		system(buf);
 	}
-	printf("[]={\n");
-	unsigned char c[1024];
-	for (i = 0; j = fread(c, 1, 1024, fp); i += j) {
-		for (int k = 0; k < j; k++)	printf("%d,", c[k]);
-	}
-	printf("\n};\nsize_t ");
-	for (j = 0; argv[1][j] != '.'; j++) {
-		printf("%c", argv[1][j]);
-	}
-	printf("_len=%d;\n", i); fflush(stdout);
-	fclose(fp);
 }
