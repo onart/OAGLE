f83279e016624adcd24d0355d0c4acad1e9997ae
onart <onart97@naver.com>
2022-01-04 15:23:46 +0900

    2D 애니메이션

--- src/OA_Anim.cpp
+++ src/OA_Anim.cpp
@@ -1,5 +1,62 @@
 #include "OA_Anim.h"
+#include "OA_Shader.h"
+#include "OA_Entity.h"
+
+extern onart::Shader program3;
 
 namespace onart {
 	std::map<std::string, Animation*> Animation::animations;
+
+	Animation::Animation(bool loop, float duration, int staticTps, float dynamicTps) :loop(loop), duration(duration), staticTps(staticTps), dynamicTps(dynamicTps) {
+		
+	}
+
+	Animation2D::Animation2D(bool loop, const std::map<float, unsigned>& tex, const std::map<float, vec4>& rects, const std::map<float, vec2>& pivots)
+		: tex(tex), rects(rects), pivots(pivots), Animation(loop, rects.empty() ? 0 : rects.rbegin()->first) {
+		hasPivot = pivots.size();
+		if (!rects.empty()) {
+			auto v = rects.begin()->second;
+			wh0.x = v.z; wh0.y = v.w;
+		}
+	}
+
+	void Animation2D::go(float elapsed, Entity* e) {
+		float tp = getTp(elapsed);
+
+		auto tlb = std::prev(tex.upper_bound(tp));
+		if (tlb != tex.end()) {
+			program3.texture(tlb->second);
+		}
+
+		auto rlb = std::prev(rects.upper_bound(tp));
+		if (rlb != rects.end()) {
+			program3.uniform("useFull", false);
+			program3.uniform("ldwh", rlb->second);
+			int i = (int)std::distance(rects.begin(), rlb);
+			e->act(i);
+		}
+		else {
+			program3.uniform("useFull", true);
+		}
+
+		if (hasPivot) {
+			auto plb = std::prev(pivots.upper_bound(tp));
+			// 베이스 직사각형 중심: (0,0), 양극단: +-0.5
+			vec2 pivv = -(plb->second); pivv += 0.5f;
+			vec2 xy(rlb->second.z, rlb->second.w);
+			
+			/// 중심이동 후 크기변환
+			mat4 pivMat(
+				xy.x, 0, 0, xy.x * pivv.x,
+				0, xy.y, 0, xy.y * pivv.y,
+				0, 0, 1, 0,
+				0, 0, 0, 1
+			);
+			program3.uniform("nopiv", false);
+			program3.uniform("piv", pivMat);
+		}
+		else {
+			program3.uniform("nopiv", true);
+		}
+	}
 }
--- src/OA_Anim.h
+++ src/OA_Anim.h
@@ -1,12 +1,14 @@
 #ifndef __OA_ANIM_H__
 #define	__OA_ANIM_H__
 
+#include "oaglem.h"
+
 #include <vector>
 #include <map>
 #include <string>
 
 namespace onart {
-	
+
 	class Entity;
 
 	/// <summary>
@@ -17,6 +19,9 @@ namespace onart {
 	{
 	public:
 		virtual void go(float elapsed, Entity* e) = 0;
+	protected:
+		Animation(bool loop, float duration, int staticTps = 1, float dynamicTps = 1);
+		inline float getTp(float elapsed) { if (duration == 0)return 0; return loop ? fmodf(elapsed, duration) : elapsed; }
 	private:
 		float duration;
 		int staticTps = 1;
@@ -27,18 +32,32 @@ namespace onart {
 
 	/// <summary>
 	/// 2D 개체의 컷 애니메이션입니다.
-	/// 2D 개체의 시각적 키포인트는 2종류로, (텍스처,시각), (텍스처 내 직사각형과 피벗, 시각)입니다. (텍스처,시각)의 경우 개체로 보낼 키포인트 넘버와는 무관합니다.
-	/// 피벗이 설정된 경우 피벗을 기준으로 2D 텍스처가 그려지는 직사각형 크기가 자동으로 조절됩니다. 설정되지 않은 경우 2D 텍스처가 그려지는 직사각형 크기는 변하지 않습니다.
+	/// 2D 개체의 시각적 키포인트는 2종류로, (텍스처,시점), (텍스처 내 직사각형과 피벗, 시점)입니다. (텍스처,시점)의 경우 개체로 보낼 키포인트 넘버와는 무관합니다.
 	/// 즉 act()는 텍스처 내 직사각형 키포인트만을 기준으로 호출됩니다.
-	/// <para>직사각형: LDWH(왼쪽/아래/가로/세로) 순서로 정의되는 vec4입니다. 좌측 하단이 (0,0), 우측 상단이 (1,1)입니다.</para>
+	/// <para>직사각형: LDWH(왼쪽/아래/가로/세로) 순서로 정의되는 vec4입니다. 원본 텍스처 이미지의 좌측 하단이 (0,0), 우측 상단이 (1,1)입니다.</para>
+	/// 피벗이 설정된 경우 피벗을 기준으로 2D 텍스처가 그려지는 직사각형 크기가 해당 시점의 rect를 기준으로 자동 조절되고 피벗이 회전/크기 변환의 중심이 됩니다. 이에 따라
+	/// 별개의 이미지에서 하나의 애니메이션으로 끌어올 경우 이미지가 차지하는 비율을 같게 맞추거나 개체의 모델을 따로 조절할 필요가 있습니다.
+	/// 설정되지 않은 경우 직사각형 크기는 변하지 않으며 변환의 중심은 그대로 프레임 직사각형의 중심이 됩니다.
+	/// 피벗은 애니메이션의 각 프레임의 크기(rects)가 같지 않게 그려진 경우에 명시하는 것이 좋습니다. 피벗은 텍스처 내 직사각형의 좌측 하단을 (0,0), 우측 상단을 (1,1)으로 합니다.
+	/// 
 	/// 생성자에서 텍스처 입력은 가독성상 Material::genTextureFromMemory() 함수 이용이 권장됩니다.
 	/// 
 	/// </summary>
 	class Animation2D: public Animation {
 	public:
-		Animation2D(std::string name);
+		/// <summary>
+		/// 애니메이션을 생성합니다.
+		/// </summary>
+		/// <param name="loop">루프 여부를 선택합니다. false인 경우 애니메이션이 끝나면 마지막 상태를 유지합니다.</param>
+		/// <param name="tex">시점과 텍스처의 순서쌍 집합입니다.</param>
+		/// <param name="rects">시점과 직사각형 영역의 순서쌍 집합입니다. 비어 있으면 안 됩니다.</param>
+		/// <param name="pivots">피벗과 직사각형 영역의 순서쌍 집합입니다.</param>
+		Animation2D(bool loop, const std::map<float, unsigned>& tex, const std::map<float, vec4>& rects, const std::map<float, vec2>& pivots = {});
 		void go(float elapsed, Entity* e);
 	private:
+		std::map<float, unsigned> tex;
+		std::map<float, vec4> rects;
+		std::map<float, vec2> pivots;
 		bool hasPivot;
 	};
 
